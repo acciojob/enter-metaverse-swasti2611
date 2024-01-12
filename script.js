2945fd27b626676929cb5fea9aac9cd1c3a02374
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-12 11:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+ let ptag=document.getElementById("status");
+       let btn=document.getElementById("enterBtn");
+     
+      btn.addEventListener("click",(e)=>{
+        let h1=document.createElement("h1");
+	h1.innerText="Entered Metaverse";
+	ptag.appendChild(h1);
+})
